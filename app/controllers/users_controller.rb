bf642c4b977628f924a85a24564c8b87899dc197
takoNkmt01 <0cto.pass0512@gmail.com>
2020-04-22 00:12:09 +0900

    add crud handling for user model without authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,47 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
+
   def index
     @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = "ユーザー登録が完了しました。"
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
   end
 
   def show
   end
 
-  def new
+  def edit
   end
 
-  def edit
+  def update
+    if @user.update(user_params)
+      flash[:success] = "ユーザー情報を更新しました。"
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :profile)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_20_052552) do
+ActiveRecord::Schema.define(version: 2020_04_21_060131) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", null: false
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_04_20_052552) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
   end
 
 end
