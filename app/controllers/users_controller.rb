25c312892f38bf5db6399811ca5bb00f46d5c5d1
takoNkmt01 <0cto.pass0512@gmail.com>
2020-04-24 22:58:53 +0900

    #11 add kaminari for pagination in the index.html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :require_admin, only: [:destroy]
 
   def index
-    @users = User.all
+    @users = User.all.page(params[:page]).per(3)
   end
 
   def new
