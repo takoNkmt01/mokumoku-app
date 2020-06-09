7880410a9b3cbd906aa5bba926c3d7d00ecd75cb
takoNkmt01 <0cto.pass0512@gmail.com>
2020-06-09 18:09:07 +0900

    #77 move helper to model

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -3,9 +3,4 @@ module CommentsHelper
   def count_reply_comments(target_comment)
     Comment.where(reply_to: target_comment.id).count
   end
-
-  # select returned comments
-  def select_returned_comments(target_comment)
-    Comment.where(reply_to: target_comment.id)
-  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,11 @@ class Comment < ApplicationRecord
   validates :user_id, presence: true
   validates :event_id, presence: true
   validates :text, presence: true
+
+  scope :reply_count, ->(target_comment) { where(reply_to: target_comment.id) }
+
+  # select returned comments
+  def self.select_returned_comments(target_comment)
+    Comment.reply_count(target_comment)
+  end
 end
