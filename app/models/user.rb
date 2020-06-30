9ee0072f407917397c4fa343d59b6f07db71e26c
takoNkmt01 <0cto.pass0512@gmail.com>
2020-06-30 11:04:26 +0900

    #114 push other file

--- app/models/user.rb
+++ app/models/user.rb
@@ -59,4 +59,17 @@ class User < ApplicationRecord
   def following?(target_user)
     following.include?(target_user)
   end
+
+  def create_notification_follow!(current_user)
+    temp = Notification.where(
+      ["visitor_id = ? and visited_id = ? and action = ?", current_user.id, id, 'follow']
+    )
+    return unless temp.blank?
+
+    notification = current_user.active_notifications.new(
+      visited_id: id,
+      action: 'follow'
+    )
+    notification.save if notification.valid?
+  end
 end
